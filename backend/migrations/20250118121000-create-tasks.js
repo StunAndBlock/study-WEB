5621e97f5fb38808833dfcec57f7bf87d46936b4
StunAndBlock <dword.byteme@mail.ru>
2025-01-19 03:10:16 +0300

    m/backend/migrations/*: [WIP]Added migrations (init tables)

---
+++ backend/migrations/20250118121000-create-tasks.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      mechanicId: {
+        type: Sequelize.STRING,
+        references: {
+          model: 'Mechanics',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      carBrand: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      operation: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      complexity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  },
+};
